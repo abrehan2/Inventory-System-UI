12034f89655c8509d1927270ae4bcedf8cf872cb
abrehan2 <rehannajam2@gmail.com>
2023-06-26 19:41:08 +0500

    Modified index.js and App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,51 @@
-import React from 'react';
+// IMPORTS -
+import React, { Suspense, useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import Navbar from "./components/Navbar.jsx";
+import Footer from "./components/Footer.jsx";
+import webfont from "webfontloader";
+import { useDispatch, useSelector } from "react-redux";
+import { loadUser } from "./redux/actions/userAction.jsx";
+import Loader from "./components/Loader.jsx";
 import "./styles/App.css";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const dispatch = useDispatch();
+  const { isAuth, loading } = useSelector((state) => state.USER);
+  const navigate = useNavigate();
 
-export default App
+  useEffect(() => {
+    // FONT LOADER -
+    webfont.load({
+      google: {
+        families: ["Roboto"],
+      },
+    });
+
+    dispatch(loadUser());
+  }, [dispatch]);
+
+  return (
+    <>
+      {loading ? (
+        <Loader />
+      ) : (
+        <Suspense fallback={<Loader />}>
+          {!isAuth ? (
+            navigate("/auth", {
+              replace: true,
+            })
+          ) : (
+            <div className="App">
+              <Navbar />
+              <Outlet />
+              <Footer />
+            </div>
+          )}
+        </Suspense>
+      )}
+    </>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
+// IMPORTS -
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { RouterProvider } from "react-router-dom";
+import Route from "./routes/Route";
+import Store from "./redux/Store";
+import { Provider } from "react-redux";
+import { ChakraProvider } from "@chakra-ui/react";
+import { Toaster } from "react-hot-toast";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <>
-    <App />
-  </>
+  <ChakraProvider>
+    <Provider store={Store}>
+      <RouterProvider router={Route} />
+      <Toaster />
+    </Provider>
+  </ChakraProvider>
 );
